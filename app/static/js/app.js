dcb57fd22c946e58f13b2f3b2900c583b66c010d
SamirAk <44325916+SamirPS@users.noreply.github.com>
2020-12-29 00:33:02 +0100

    Update app.js

--- app/static/js/app.js
+++ app/static/js/app.js
@@ -1,6 +1,6 @@
 if ('serviceWorker' in navigator) {
   navigator.serviceWorker
-  .register('./service-worker.js')
+  .register('/sw.js')
   .then(function(registration) {
       console.log('Service Worker Registered!');
       return registration;
