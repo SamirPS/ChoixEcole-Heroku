a4d68fe823eaf3d0659e11516d68dc385274805b
SamirAk <44325916+SamirPS@users.noreply.github.com>
2020-12-29 11:34:16 +0100

    Update main.py

--- app/controller/main.py
+++ app/controller/main.py
@@ -1,7 +1,4 @@
 from flask import Flask,Blueprint, session, redirect, url_for, request,render_template
-import sys
-sys.path.append('/app/controller/')
-
 import calcul
 
 bp = Blueprint('main', __name__)
