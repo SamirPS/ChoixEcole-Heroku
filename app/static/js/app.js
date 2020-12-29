d2cac51d21ff5985137706ee8a892505ace024e3
SamirAk <44325916+SamirPS@users.noreply.github.com>
2020-12-29 11:29:36 +0100

    Update app.js

--- app/static/js/app.js
+++ app/static/js/app.js
@@ -85,17 +85,3 @@ function logAppInstalled(evt) {
   console.log('App was installed.', evt);
 }
 
-function resizeToMinimum(){
-  var minimum    = [640, 480];
-  var current    = [window.outerWidth, window.outerHeight];
-  var restricted = [];
-  var i          = 2;
-
-  while(i-- > 0){
-    restricted[i] = minimum[i] > current[i] ? minimum[i] : current[i];
-  }
-
-  window.resizeTo(current[0], current[1]);
-}
-
-window.addEventListener('resize', resizeToMinimum, false);
