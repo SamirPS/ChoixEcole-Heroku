10bc6b470886785e46d7a3d9af570570d82370b9
SamirAk <44325916+SamirPS@users.noreply.github.com>
2021-01-01 17:14:49 +0100

    Update sw.js

--- app/static/sw.js
+++ app/static/sw.js
@@ -5,14 +5,6 @@ importScripts('https://storage.googleapis.com/workbox-cdn/releases/3.2.0/workbox
 if (workbox) {
   console.log(`Yay! Workbox is loaded 🎉`);
 
-
-  workbox.routing.registerRoute(
-    /\.(?:js|css)$/,
-     workbox.strategies.staleWhileRevalidate({
-      cacheName: 'static-resources',
-    }),
-  );
-
   workbox.routing.registerRoute(
     /\.(?:png|gif|jpg|jpeg|svg)$/,
     workbox.strategies.cacheFirst({
